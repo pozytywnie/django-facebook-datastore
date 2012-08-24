63bae08c9b11d53c8bfa34c1e0ef42561f2e7b1d
Piotr Maliński <riklaunim@gmail.com>
2012-08-24 16:55:59 +0200

    Add django project.
    Change-Id: I5792130a9f89db02f4ef3ed0529789e71370a0dc

---
+++ engines/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Dummy test for Jenkins
+        """
+        self.assertEqual(1 + 1, 2)
