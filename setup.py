1686db82fdb983201c7aeb38d0bc9ecb0f5697f8
Piotr Maliński <riklaunim@gmail.com>
2012-08-24 16:40:09 +0200

    Add Manifest and setup.py
    Change-Id: I780aff464a1737f66ceaa16ee2cbd16ccbfce3d7

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='facebook-datastore',
+    version='0.1',
+    description="Facebook Datastore",
+    maintainer="Tomasz Wysocki",
+    maintainer_email="tomasz@wysocki.info",
+    install_requires=(
+        'django==1.4',
+        'facebook-javascript-authentication',
+        'facebook-javascript-sdk',
+        'south==0.7.5',
+    ),
+    packages=find_packages(),
+    include_package_data=True,
+)
