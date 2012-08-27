f567a4c862f238f2e88d6cbc201ea4d204ceba2b
Piotr Maliński <riklaunim@gmail.com>
2012-08-27 11:53:47 +0200

    Import Facebook user profile from pozytywka.
    Change-Id: I84ea930124361b3b6c7e9a267ea66bc35ee2ccc4
    Reviewed-on: http://review.pozytywnie.pl:8080/5931
    Tested-by: Jenkins
    Reviewed-by: Jenkins

---
+++ facebook_datastore/models.py
@@ -0,0 +1,56 @@
+import logging
+
+from django.db import models
+
+
+logger = logging.getLogger(__name__)
+
+
+class FacebookUserProfileManager(models.Manager):
+    def create_or_update(self, parameters):
+        """Parameters must include 'facebook_id' key."""
+        try:
+            instance = self.get(facebook_id=parameters['facebook_id'])
+        except FacebookUserProfile.DoesNotExist:
+            return FacebookUserProfile(**parameters)
+        instance.update(parameters)
+        return instance
+
+
+class FacebookUserProfile(models.Model):
+    GENDERS = (
+        ('m', 'Male'),
+        ('f', 'Female'),
+    )
+
+    user = models.ForeignKey('auth.User', null=True, blank=True, unique=True)
+
+    facebook_id = models.BigIntegerField(unique=True)
+    access_token = models.TextField(null=True, blank=True)
+
+    name = models.TextField(null=True, blank=True)
+    first_name = models.TextField(null=True, blank=True)
+    last_name = models.TextField(null=True, blank=True)
+    middle_name = models.TextField(null=True, blank=True)
+    username = models.TextField(null=True, blank=True)
+    email = models.EmailField(null=True, blank=True, max_length=254)
+
+    gender = models.CharField(max_length=1, choices=GENDERS, null=True, blank=True)
+    locale = models.CharField(max_length=5, null=True, blank=True)
+    link = models.URLField(null=True, blank=True)
+    birthday = models.DateField(null=True, blank=True)
+    location_name = models.TextField(null=True, blank=True)
+    location_id = models.TextField(null=True, blank=True)
+    relationship_status = models.TextField(null=True, blank=True)
+    website = models.TextField(null=True, blank=True)
+    about_me = models.TextField(blank=True, null=True)
+    raw_data = models.TextField(blank=True, null=True)
+
+    objects = FacebookUserProfileManager()
+
+    def update(self, parameters):
+        for key, value in parameters.iteritems():
+            if hasattr(self, key):
+                setattr(self, key, value)
+            else:
+                logger.warning("FacebookUserProfile has no '%s' atribute" % key)
