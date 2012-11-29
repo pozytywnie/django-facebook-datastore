ce849b06be86f853b06fa8abb7e7a2ede55ebe7e
Piotr Maliński <piotr@pozytywnie.pl>
2012-11-29 16:46:54 +0100

    Bump facebook-datastore version to 0.1.1.
    Change-Id: I3772c96287acd72bd214c2f5952fa092ffd70b71
    Reviewed-on: http://review.pozytywnie.pl:8080/8250
    Reviewed-by: Jenkins
    Reviewed-by: Tomasz Wysocki <tomwys@gmail.com>
    Tested-by: Tomasz Wysocki <tomwys@gmail.com>

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='facebook-datastore',
-    version='0.1',
+    version='0.1.1',
     description="Facebook Datastore",
     maintainer="Tomasz Wysocki",
     maintainer_email="tomasz@wysocki.info",
